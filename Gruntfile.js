1284c37bcadde2e99f17d2b2e87f3f31848577b5
Alasdair Mercer <mercer.alasdair@gmail.com>
2018-11-22 21:41:03 +0000

    moved from !ninja to neocotic

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Alasdair Mercer, !ninja
+ * Copyright (C) 2018 Alasdair Mercer
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
           moduleName: 'pollock',
           sourceMap: true,
           sourceMapRelativePaths: true,
-          banner: '/*! pollock v<%= pkg.version %> | (C) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>, !ninja | <%= pkg.license %> License */',
+          banner: '/*! pollock v<%= pkg.version %> | (C) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> | <%= pkg.license %> License */',
           plugins: function() {
             return [
               nodeResolve(),
